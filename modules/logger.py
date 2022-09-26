4d0b0155c2c28eb688c57c8ff2edec8bb96e3390
Timur Ratnikov <timur2640rt@gmail.com>
2022-09-26 19:58:43 +0300

    1

--- modules/logger.py
+++ modules/logger.py
@@ -2,7 +2,7 @@ import logging
 import logging.config
 import yaml
 
-source_path = '/home/pi/raspdev'
+source_path = ''
 with open(source_path + '/modules/loggin-config.yml', 'r') as logconf:
     logging_config = yaml.load(logconf, Loader=yaml.FullLoader)
 logging_config.config.dictConfig(config)
